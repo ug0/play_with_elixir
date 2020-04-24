1d3ebadcd1c3cf71987672f043b8c2acc61eed04
ug0 <ug600@icloud.com>
2020-04-24 15:20:29 +0800

    Update Todo.Database

--- todo/lib/todo/database.ex
+++ todo/lib/todo/database.ex
@@ -10,41 +10,35 @@ defmodule Todo.Database do
   end
 
   def store(key, data) do
-    GenServer.cast(__MODULE__, {:store, key, data})
+    key
+    |> choose_worker()
+    |> DatabaseWorker.store(key, data)
   end
 
   def get(key) do
-    GenServer.call(__MODULE__, {:get, key})
+    key
+    |> choose_worker()
+    |> DatabaseWorker.get(key)
+  end
+
+  defp choose_worker(key) do
+    GenServer.call(__MODULE__, {:choose_worker, key})
   end
 
   def init(_) do
     File.mkdir_p!(@db_folder)
 
-    state = 0..2
-    |> Enum.reduce(%{}, fn i, workers ->
+    workers = Enum.into(0..2, %{}, fn i ->
       {:ok, worker} = DatabaseWorker.start(@db_folder)
-      Map.put(workers, i, worker)
+      {i, worker}
     end)
 
-    {:ok, state}
+    {:ok, workers}
   end
 
-  def handle_cast({:store, key, data}, state) do
-    choose_worker(state, key)
-    |> DatabaseWorker.store(key, data)
+  def handle_call({:choose_worker, key}, _caller, workers) do
+    worker = Map.fetch!(workers, :erlang.phash2(key, 3))
 
-    {:noreply, state}
-  end
-
-  def handle_call({:get, key}, _caller, state) do
-    data =
-      choose_worker(state, key)
-      |> DatabaseWorker.get(key)
-
-    {:reply, data, state}
-  end
-
-  def choose_worker(state, key) do
-    Map.fetch!(state, :erlang.phash2(key, 3))
+    {:reply, worker, workers}
   end
 end
